42fa6fe412d90bd6fb4db4d31349f53923ec9f31
Alexander Lubovsky <alexander.lubovsky@intexsoft.by>
2022-07-05 11:43:58 +0300

    Add product serializer

--- products/serializers.py
+++ products/serializers.py
@@ -1,8 +1,17 @@
+from decimal import Decimal
+
 from rest_framework import serializers
 
+from products.models import Product
 
-class ProductSerializer(serializers.HyperlinkedModelSerializer):
+
+class ProductSerializer(serializers.Serializer):
     id = serializers.IntegerField()
     title = serializers.CharField(max_length=250)
     description = serializers.CharField()
-    price = serializers.IntegerField()
+    unit_price = serializers.IntegerField(source='price')
+    price_with_tax = serializers.SerializerMethodField(method_name='calculate_tax')
+
+    @staticmethod
+    def calculate_tax(product: Product) -> Decimal:
+        return product.price * Decimal(1.08)
--- products/views.py
+++ products/views.py
@@ -1,17 +1,20 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from django.shortcuts import get_object_or_404
 from .models import Product
 from .serializers import ProductSerializer
 
 
 @api_view(http_method_names=['GET'])
 def product_list(request):
-    return Response('ok')
+    queryset = Product.objects.all()
+    serializer = ProductSerializer(queryset, many=True)
+
+    return Response(serializer.data)
 
 
 @api_view(http_method_names=['GET'])
 def product_detail(request, id):
-    product = Product.objects.get(pk=id)
+    product = get_object_or_404(Product, pk=id)
     serializer = ProductSerializer(product)
-
     return Response(serializer.data)

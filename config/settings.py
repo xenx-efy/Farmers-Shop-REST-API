9a3aab1989448d83927fdb66624bc7e26b64a41a
Alexander Lubovsky <alexander.lubovsky@intexsoft.by>
2022-07-07 11:16:59 +0300

    Add custom serializer for user creation endpoint

--- config/settings.py
+++ config/settings.py
@@ -143,3 +143,9 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = 'core.User'
+
+DJOSER = {
+    'SERIALIZERS': {
+        'user_create': 'core.serializers.UserCreateSerializer'
+    }
+}

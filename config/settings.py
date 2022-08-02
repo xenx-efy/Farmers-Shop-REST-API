5e544de25c301f42d42cd61edbeeaa39dddaf7ff
Alexander Lubovsky <alexander.lubovsky@intexsoft.by>
2022-08-02 22:20:09 +0300

    Add pagination setting

--- config/settings.py
+++ config/settings.py
@@ -29,6 +29,8 @@ ALLOWED_HOSTS = []
 REST_FRAMEWORK = {
     "COERCE_DECIMAL_TO_STRING": False,
     "DEFAULT_AUTHENTICATION_CLASSES": ("rest_framework_simplejwt.authentication.JWTAuthentication",),
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
+    'PAGE_SIZE': 100
 }
 
 SIMPLE_JWT = {
